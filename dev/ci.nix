a65ade35735002806a73474be0395e8cb78c2e97
Terje Larsen <terlar@gmail.com>
2026-04-04 15:03:32 +0200

    feat: use plan in apply

--- dev/ci.nix
+++ dev/ci.nix
@@ -204,8 +204,16 @@ in
               }
               {
                 name = "tofu plan";
-                run = "tofu plan -no-color -input=false";
+                run = "tofu plan -no-color -input=false -out=tfplan";
                 env = gitEnv;
+              }
+              {
+                name = "Upload plan";
+                uses = "actions/upload-artifact@v4";
+                "with" = {
+                  name = "tfplan";
+                  path = "terraform/tfplan";
+                };
               }
             ];
           };
@@ -220,13 +228,21 @@ in
             defaults.run.working-directory = "terraform";
             steps = lib.mkAfter [
               {
+                name = "Download plan";
+                uses = "actions/download-artifact@v4";
+                "with" = {
+                  name = "tfplan";
+                  path = "terraform";
+                };
+              }
+              {
                 name = "tofu init";
                 run = "tofu init -input=false";
                 env = gitEnv;
               }
               {
                 name = "tofu apply";
-                run = "tofu apply -auto-approve -input=false";
+                run = "tofu apply -input=false tfplan";
                 env = gitEnv;
               }
             ];
